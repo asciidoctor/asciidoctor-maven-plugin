9650aaf618a7f6331045580d1288fa272e782c86
LightGuard <lightguard.jp@gmail.com>
2013-01-24 20:39:35 -0700

    Testing HTML output

--- src/test/groovy/org/asciidoc/maven/test/AsciidoctorMojoTest.groovy
+++ src/test/groovy/org/asciidoc/maven/test/AsciidoctorMojoTest.groovy
@@ -44,4 +44,25 @@ class AsciidoctorMojoTest extends Specification {
             File sampleOutput = new File('sample.xml', outputDir)
             sampleOutput.length() > 0
     }
+
+    def "renders html"() {
+        when:
+            File srcDir = new File('target/test-classes/src/asciidoc')
+            File outputDir = new File('target/asciidoc-output')
+
+            if (!outputDir.exists())
+                outputDir.mkdir()
+
+            AsciidoctorMojo mojo = new AsciidoctorMojo()
+            mojo.backend = 'html'
+            mojo.sourceDirectory = srcDir
+            mojo.outputDirectory = outputDir
+            mojo.execute()
+        then:
+            outputDir.list().toList().isEmpty() == false
+            outputDir.list().toList().contains('sample.html')
+
+            File sampleOutput = new File('sample.html', outputDir)
+            sampleOutput.length() > 0
+    }
 }
