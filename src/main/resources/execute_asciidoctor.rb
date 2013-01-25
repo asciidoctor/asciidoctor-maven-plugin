becf1dc43b1b42d435d219a3e1fd30f82c621f72
LightGuard <lightguard.jp@gmail.com>
2013-01-24 21:13:29 -0700

    Preparing for Asciidoctor 0.1.0

--- src/main/resources/execute_asciidoctor.rb
+++ src/main/resources/execute_asciidoctor.rb
@@ -1,11 +1,10 @@
 require 'asciidoctor'
+require 'asciidoctor/cli/invoker'
+require 'asciidoctor/cli/options'
+require 'find'
 
-asciidoctor_opts = {:safe => Asciidoctor::SafeMode::UNSAFE, :base_dir => $srcDir, :backend => $backend}
-Dir.new($srcDir).each do |file|
-  file_ext = File.extname(file.to_s)
-  if file_ext === '.adoc' or file_ext === '.asciidoc' or file_ext === '.asc' or file_ext === '.ad'
-    basename = File.basename(file.to_s, file_ext) + ($backend === 'docbook' ? '.xml' : '.html')
-    rendered_output = Asciidoctor::Document.new(file.lines.to_a, asciidoctor_opts).render
-    File.open($outputDir + '/' + basename, 'w') { |f| f.write(rendered_output) }
+Find.find($srcDir) do |path|
+  if path =~ /.*\.a((sc(iidoc)?)|d(oc)?)$/
+    Asciidoctor::Cli::Invoke.new({:safe => Asciidoctor::SafeMode::UNSAFE, :base_dir => $srcDir, :backend => $backend}).invoke!
   end
 end
