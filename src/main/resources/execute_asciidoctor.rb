ff2afbfda966e7394612bfb4466c065e10f3774b
LightGuard <lightguard.jp@gmail.com>
2013-01-25 09:19:29 -0700

    Switching to a better API for 0.1.0

--- src/main/resources/execute_asciidoctor.rb
+++ src/main/resources/execute_asciidoctor.rb
@@ -1,10 +1,9 @@
 require 'asciidoctor'
-require 'asciidoctor/cli/invoker'
-require 'asciidoctor/cli/options'
 require 'find'
 
 Find.find($srcDir) do |path|
   if path =~ /.*\.a((sc(iidoc)?)|d(oc)?)$/
-    Asciidoctor::Cli::Invoke.new({:safe => Asciidoctor::SafeMode::UNSAFE, :base_dir => $srcDir, :backend => $backend}).invoke!
+    Asciidoctor.render_file({path, :in_place => true, :safe => Asciidoctor::SafeMode::UNSAFE,
+                             :base_dir => $srcDir, :backend => $backend})
   end
 end
