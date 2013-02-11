6ad2906f4221cf7796ebe12e3466c1f479f7a4de
LightGuard <lightguard.jp@gmail.com>
2013-02-11 12:18:02 -0700

    Updating to the new 0.1.0 and bundler

--- src/main/resources/execute_asciidoctor.rb
+++ src/main/resources/execute_asciidoctor.rb
@@ -1,9 +1,9 @@
+require 'bundler/setup'
 require 'asciidoctor'
-require 'find'
 
-Find.find($srcDir) do |path|
+Dir.glob("#{$srcDir}/**/*.a*").each do |path|
   if path =~ /.*\.a((sc(iidoc)?)|d(oc)?)$/
-    Asciidoctor.render_file({path, :in_place => true, :safe => Asciidoctor::SafeMode::UNSAFE,
-                             :base_dir => $srcDir, :backend => $backend})
+    Asciidoctor.render_file(path, {:in_place => false, :safe => Asciidoctor::SafeMode::UNSAFE,
+                                   :attributes => {'backend' => $backend}, :to_dir => $outputDir})
   end
 end
