e653567a8462c589a891de18b6f8855b63c5e99d
Jason Lee <jason@steeplesoft.com>
2013-04-29 22:17:35 -0500

    Clean up commented-out code Add site usage documentation

--- src/main/java/org/asciidoc/maven/site/AsciidoctorParser.java
+++ src/main/java/org/asciidoc/maven/site/AsciidoctorParser.java
@@ -21,16 +21,13 @@ import org.codehaus.plexus.util.IOUtil;
  *
  * @author jdlee
  */
-@Component( role = Parser.class, hint = "asciidoc" )
+@Component( role = Parser.class, hint = AsciidoctorParser.ROLE_HINT )
 public class AsciidoctorParser extends XhtmlParser {
 
     /**
      * The role hint for the {@link MarkdownParser} Plexus component.
      */
     public static final String ROLE_HINT = "asciidoc";
-    /**
-     * The {@link PegDownProcessor} used to convert Pegdown documents to HTML.
-     */
     protected final Asciidoctor asciidoctorInstance = Asciidoctor.Factory.create();
 
     /**
@@ -39,11 +36,9 @@ public class AsciidoctorParser extends XhtmlParser {
     @Override
     public void parse(Reader source, Sink sink) throws ParseException {
         try {
-            System.out.println("Parsing!");
-            String adoc = asciidoctorInstance.render(IOUtil.toString(source), new HashMap());
-//            RootNode rootNode = PEGDOWN_PROCESSOR.parseMarkdown(IOUtil.toString(source).toCharArray());
-//            String adocToHtml = new AsciidocToDoxiaHtmlSerializer().toHtml(rootNode);
-            super.parse(new StringReader("<html><body>" + adoc + "</body></html>"), sink);
+            super.parse(new StringReader("<html><body>" +
+                    asciidoctorInstance.render(IOUtil.toString(source), new HashMap())+
+                    "</body></html>"), sink);
         } catch (IOException e) {
             throw new ParseException("Failed reading Asciidoc source document", e);
         }
