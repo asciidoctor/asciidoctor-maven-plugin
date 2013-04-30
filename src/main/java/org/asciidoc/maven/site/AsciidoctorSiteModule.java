6af24d4144fcbcf20c70a50ebff22e92d6660dbb
Jason Lee <jason@steeplesoft.com>
2013-04-29 22:46:58 -0500

    Clean up documenation in the README Remove extraneous Markdown references in comments Add some basic class docs Add copyright header (copied to match the rest of the project) Change the file extension per Jason's request     (it does not appear to support multiple extensions. Multiple SiteModules might work around that)

--- src/main/java/org/asciidoc/maven/site/AsciidoctorSiteModule.java
+++ src/main/java/org/asciidoc/maven/site/AsciidoctorSiteModule.java
@@ -1,6 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.asciidoc.maven.site;
 
@@ -9,23 +16,24 @@ import org.apache.maven.doxia.module.site.SiteModule;
 import org.codehaus.plexus.component.annotations.Component;
 
 /**
+ * This class is the entry point for the site plugin integration.
  *
  * @author jdlee
  */
-@Component( role = SiteModule.class, hint = AsciidoctorParser.ROLE_HINT )
+@Component(role = SiteModule.class, hint = AsciidoctorParser.ROLE_HINT)
 public class AsciidoctorSiteModule extends AbstractSiteModule {
 
     /**
-     * The source directory for Markdown files.
+     * The source directory for asciidoc files.
      */
     public static final String SOURCE_DIRECTORY = "asciidoc";
     /**
-     * The extension for Markdown files.
+     * The extension for asciidoc files.
      */
-    public static final String FILE_EXTENSION = "adoc";
+    public static final String FILE_EXTENSION = "ad";
 
     /**
-     * Build a new instance of {@link MarkdownSiteModule}.
+     * Build a new instance of {@link AsciidocSiteModule}.
      */
     public AsciidoctorSiteModule() {
         super(SOURCE_DIRECTORY, FILE_EXTENSION, AsciidoctorParser.ROLE_HINT);
