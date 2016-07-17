4333dcc508de81847bf522fd33eeff5c069bf90e
Abel Salgado Romero <abelromero@gmail.com>
2016-07-17 18:46:52 +0200

    added validations and source example

--- src/it/article-html-command/validate.groovy
+++ src/it/article-html-command/validate.groovy
@@ -11,8 +11,9 @@ for ( String expectedFile : expectedFiles ) {
 
     // validate that asciidoctor.attributes are processed
     String text = file.text
-    text.contains('<body class="article toc2 toc-right">')
-    text.contains('<pre class="CodeRay highlight">')
+    if ( !text.contains('<body class="article toc2 toc-left">') || !text.contains('<pre class="CodeRay highlight">') ) {
+        throw new Exception( "Attributes not processed" )
+    }
 
     if ( !file.isFile() ) {
         throw new Exception( "Missing file " + file )
