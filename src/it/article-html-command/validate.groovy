1bbf060ace01bdbc4b03f1db2806f5fd97624a54
Abel Salgado Romero <abelromero@gmail.com>
2016-07-17 18:58:24 +0200

    Added additional validations to integration test 'article-html-command' (#237)
    * added attributes validation to `article-html-command` integration test
    
    * added validations and source example

--- src/it/article-html-command/validate.groovy
+++ src/it/article-html-command/validate.groovy
@@ -8,6 +8,13 @@ String[] expectedFiles = ["sample.html"]
 for ( String expectedFile : expectedFiles ) {
     File file = new File( outputDir, expectedFile )
     println ( "Checking for existence of " + file )
+
+    // validate that asciidoctor.attributes are processed
+    String text = file.text
+    if ( !text.contains('<body class="article toc2 toc-left">') || !text.contains('<pre class="CodeRay highlight">') ) {
+        throw new Exception( "Attributes not processed" )
+    }
+
     if ( !file.isFile() ) {
         throw new Exception( "Missing file " + file )
     }
