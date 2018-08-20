fd9959c13e977dda99c0ebd8913164071fed21e5
Tim Krüger <tim.krueger@logiball.de>
2018-08-20 15:28:28 +0200

    Add integration test for thread safe lable
    All three modules produces the same result. If the content
    is mixed during the parallel execution the results will
    be different and the test fails.

---
+++ src/it/thread-safe/validate.groovy
@@ -0,0 +1,26 @@
+import java.nio.file.Files
+
+String fileTemplate = "asciidoctor-project-%s/target/docs/sample.html"
+
+Set<String> contentOfFiles = new HashSet<>()
+
+for (int i = 1; i <= 3; i++) {
+    File file = new File(basedir, String.format(fileTemplate, i))
+    println("Checking for existence of " + file)
+    if (!file.isFile()) {
+        throw new Exception("Missing file " + file)
+    }
+
+    StringBuilder contentOfFile = new StringBuilder()
+    for (String line : Files.readAllLines(file.toPath())) {
+        contentOfFile.append(line)
+    }
+    contentOfFiles.add(contentOfFile.toString())
+}
+
+if (contentOfFiles.size() == 1) {
+    return true
+} else {
+    throw new Exception("All files must contain the same content")
+}
+
