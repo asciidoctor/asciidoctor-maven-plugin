2cca1e8e4da77fc28df65035334107ad58998737
Tim Krüger <tim.krueger@logiball.de>
2018-08-20 16:42:06 +0200

    Don't generate header and footer
    Set 'headerFooter' to 'false'. The generated html files must
    not contain timestamps.

--- src/it/thread-safe/validate.groovy
+++ src/it/thread-safe/validate.groovy
@@ -3,8 +3,8 @@ import java.nio.file.Files
 
 String fileTemplate = "asciidoctor-project-%s/target/docs/sample.html"
 
-Set<String> contentOfFiles = new HashSet<>()
-
+List<String> contents = new ArrayList<>()
+List<String> files = new ArrayList<>()
 for (int i = 1; i <= 3; i++) {
     File file = new File(basedir, String.format(fileTemplate, i))
     println("Checking for existence of " + file)
@@ -16,12 +16,22 @@ for (int i = 1; i <= 3; i++) {
     for (String line : Files.readAllLines(file.toPath(), Charset.forName("UTF-8"))) {
         contentOfFile.append(line)
     }
-    contentOfFiles.add(contentOfFile.toString())
+    contents.add(contentOfFile.toString())
+    files.add(String.format(fileTemplate, i))
+
 }
 
-if (contentOfFiles.size() == 1) {
-    return true
-} else {
-    throw new Exception("All files must contain the same content")
+
+if(!contents.get(0).equals(contents.get(1))) {
+    throw new Exception(String.format("The content of two files are different.\n\n%s: %s\n%s: %s", files.get(0), contents.get(0), files.get(1), contents.get(1) ))
 }
 
+if(!contents.get(1).equals(contents.get(2))) {
+    throw new Exception(String.format("The content of two files are different.\n\n%s: %s\n%s: %s", files.get(1), contents.get(1), files.get(2), contents.get(2) ))
+}
+
+if(!contents.get(2).equals(contents.get(0))) {
+    throw new Exception(String.format("The content of two files are different.\n\n%s: %s\n%s: %s", files.get(2), contents.get(2), files.get(0), contents.get(0) ))
+}
+
+return true
