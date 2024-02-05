a561dbb11721fbae17282ab0ada4e387b26f175b
Abel Salgado Romero <abelromero@gmail.com>
2024-02-06 00:32:03 +0100

    Refactor and basic test skaffolding

--- src/test/java/org/asciidoctor/maven/site/SiteConverterTest.java
+++ src/test/java/org/asciidoctor/maven/site/SiteConverterTest.java
@@ -1,9 +1,15 @@
 package org.asciidoctor.maven.site;
 
 import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
 import org.asciidoctor.Options;
+import org.asciidoctor.SafeMode;
 import org.asciidoctor.maven.site.SiteConverter.Result;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.swing.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,15 +17,49 @@ class SiteConverterTest {
 
     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
 
-    @Test
-    void should_extract_title() {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "= Hello, AsciiDoc!",
+            "# Hello, AsciiDoc!",
+            "Hello, AsciiDoc!\n================"
+    })
+    void should_extract_title_from_header(String title) {
         SiteConverter siteConverter = new SiteConverter(asciidoctor);
 
-        Options options = Options.builder().build();
-        Result result = siteConverter.process("= Hello\n\n", options);
+        Options options = defaultOptions();
+        Result result = siteConverter.process(title + "\n", options);
 
-        assertThat(result.getTitle()).isEqualTo("Hello");
-        assertThat(result.getHtml()).isEqualTo("Hello");
+        assertThat(result.getTitle()).isEqualTo("Hello, AsciiDoc!");
+        assertThat(result.getHtml()).isNotBlank();
     }
 
+    @Test
+    void should_extract_title_from_attribute() {
+        SiteConverter siteConverter = new SiteConverter(asciidoctor);
+
+        Options options = Options.builder()
+                .safe(SafeMode.UNSAFE)
+                .backend("xhtml")
+                .attributes(Attributes.builder()
+                        .attribute("idprefix", "@")
+                        .attribute("showtitle", "@")
+                        .attribute("who", "me")
+                        .build())
+                .build();
+        Result result = siteConverter.process("= Hello, {who}!\n", options);
+
+        assertThat(result.getTitle()).isEqualTo("Hello, me!");
+        assertThat(result.getHtml()).isNotBlank();
+    }
+
+    private Options defaultOptions() {
+        return Options.builder()
+                .safe(SafeMode.UNSAFE)
+                .backend("xhtml")
+                .attributes(Attributes.builder()
+                        .attribute("idprefix", "@")
+                        .attribute("showtitle", "@")
+                        .build())
+                .build();
+    }
 }
