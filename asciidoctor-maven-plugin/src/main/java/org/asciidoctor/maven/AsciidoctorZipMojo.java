4aded541bea5d5c5bfa86d45f0d6adb54f2a025e
Abel Salgado Romero <abelromero@gmail.com>
2024-02-11 12:30:50 +0100

    Mark Zip mojo as deprecated for removal in next major
    Related to #145

--- asciidoctor-maven-plugin/src/main/java/org/asciidoctor/maven/AsciidoctorZipMojo.java
+++ asciidoctor-maven-plugin/src/main/java/org/asciidoctor/maven/AsciidoctorZipMojo.java
@@ -11,6 +11,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProjectHelper;
 import org.asciidoctor.maven.io.Zips;
 
+@Deprecated(since = "3.0.0", forRemoval = true)
 @Mojo(name = "zip")
 public class AsciidoctorZipMojo extends AsciidoctorMojo {
     public static final String PREFIX = AsciidoctorMaven.PREFIX + "zip.";
