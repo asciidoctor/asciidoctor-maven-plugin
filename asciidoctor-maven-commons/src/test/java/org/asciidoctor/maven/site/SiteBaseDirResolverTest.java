76ad3240be6e3d71dd6819f9010c907bb83a5256
Abel Salgado Romero <abelromero@gmail.com>
2024-11-02 10:02:47 +0100

    fix test on Windows

--- asciidoctor-maven-commons/src/test/java/org/asciidoctor/maven/site/SiteBaseDirResolverTest.java
+++ asciidoctor-maven-commons/src/test/java/org/asciidoctor/maven/site/SiteBaseDirResolverTest.java
@@ -2,20 +2,18 @@ package org.asciidoctor.maven.site;
 
 import java.io.File;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import lombok.SneakyThrows;
-import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 class SiteBaseDirResolverTest {
 
@@ -28,7 +26,7 @@ class SiteBaseDirResolverTest {
         SiteBaseDirResolver resolver = new SiteBaseDirResolver();
         File result = resolver.resolveBaseDir(BASE_DIR, siteConfiguration);
 
-        assertThat(result.toString()).isEqualTo("src/site");
+        assertThat(result.toString()).isEqualTo(path("src", "site"));
     }
 
     @ParameterizedTest
@@ -40,7 +38,7 @@ class SiteBaseDirResolverTest {
         SiteBaseDirResolver resolver = new SiteBaseDirResolver();
         File result = resolver.resolveBaseDir(new File(mavenBaseDir), siteConfiguration);
 
-        assertThat(result.toString()).isEqualTo(mavenBaseDir.equals(".") ? "my-src/my-site" : "something/my-src/my-site");
+        assertThat(result.toString()).isEqualTo(mavenBaseDir.equals(".") ? path("my-src", "my-site") : path("something", "my-src", "my-site"));
     }
 
     @Test
@@ -51,7 +49,7 @@ class SiteBaseDirResolverTest {
         SiteBaseDirResolver resolver = new SiteBaseDirResolver();
         File result = resolver.resolveBaseDir(BASE_DIR, siteConfiguration);
 
-        assertThat(result.toString()).isEqualTo("src/site/es");
+        assertThat(result.toString()).isEqualTo(path("src", "site", "es"));
     }
 
     @Test
@@ -66,14 +64,7 @@ class SiteBaseDirResolverTest {
         SiteBaseDirResolver resolver = new SiteBaseDirResolver();
         File result = resolver.resolveBaseDir(new File("some"), siteConfiguration);
 
-        assertThat(result.toString()).isEqualTo("some/my-src/my-site/es");
-    }
-
-    @SneakyThrows
-    private javax.inject.Provider<MavenProject> createMavenProjectMock() {
-        MavenProject mockProject = Mockito.mock(MavenProject.class);
-        when(mockProject.getBasedir()).thenReturn(new File("."));
-        return () -> mockProject;
+        assertThat(result.toString()).isEqualTo(path("some", "my-src", "my-site", "es"));
     }
 
     @SneakyThrows
@@ -83,6 +74,10 @@ class SiteBaseDirResolverTest {
             .map(k -> String.format("<%1$s>%2$s</%1$s>", k, config.get(k)))
             .collect(Collectors.joining());
         return Xpp3DomBuilder.build(new StringReader(String.format("<configuration>%s</configuration>", configurationsXml)));
+    }
+
+    private static String path(String... parts) {
+        return Arrays.stream(parts).collect(Collectors.joining(File.separator));
     }
 
 }
