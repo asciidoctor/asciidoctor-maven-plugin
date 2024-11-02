507361f3ca7c1e812e817610ec37bd17a768e360
Abel Salgado Romero <abelromero@gmail.com>
2024-11-02 10:33:15 +0100

    Add warning when multiple locales are found

--- asciidoctor-maven-commons/src/main/java/org/asciidoctor/maven/site/SiteBaseDirResolver.java
+++ asciidoctor-maven-commons/src/main/java/org/asciidoctor/maven/site/SiteBaseDirResolver.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.nio.file.Path;
 
 import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Calculates the path where the root of the sources are located based on the
@@ -14,6 +16,8 @@ import org.codehaus.plexus.util.xml.Xpp3Dom;
  * @since 3.1.1
  */
 public class SiteBaseDirResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(SiteBaseDirResolver.class);
 
     public static File resolveBaseDir(File mavenBaseDir, Xpp3Dom siteConfig) {
         final String siteDirectory = resolveSiteDirectory(siteConfig);
@@ -47,8 +51,11 @@ public class SiteBaseDirResolver {
         if (siteConfig != null) {
             final Xpp3Dom locales = siteConfig.getChild("locales");
             if (locales != null) {
-                // For now,2 we support 1 locale
+                // We can support 1 locale: https://issues.apache.org/jira/browse/DOXIA-755
                 String[] split = locales.getValue().split(",");
+                if (split.length > 0) {
+                    logger.warn("Multiple locales found: {}, this is not supported. Configure multiple plugin executions instead.", locales.getValue());
+                }
                 return split[0];
             }
         }
